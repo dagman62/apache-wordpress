3a660657be302f085a3b7827f731b5bf73561be5
Gaetano Giacalone <ggiacalone@gaetano-vbox3.example.com>
2018-05-06 13:13:01 -0700

    removed files that are not needed

--- configure-http.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PREFIX=/usr/local/apache
-
-./configure --prefix=${PREFIX} --enable-ssl --enable-proxy --enable-modules=all --enable-mods-shared=all --enable-module=so --enable-proxy-http --enable-proxy-balancer --enable-proxy-ajp --with-ssl --with-mpm=prefork --with-apr=${PREFIX}/bin/apr-1-config --with-apr-util=${PREFIX}/bin/apu-1-config --enable-cgi
--- configure-php.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PREFIX=/usr/local/apache
-
-./configure --with-zlib --enable-zip --enable-wddx --enable-sysvmsg --enable-sockets --enable-soap --enable-shmop --enable-mbstring --with-mhash --with-gettext --with-gd --enable-ftp --enable-exif --with-curl --enable-calendar --with-bz2 --enable-bcmath --enable-static --with-apxs2=${PREFIX}/bin/apxs --with-mysqli --enable-embedded-mysqli --enable-dba
